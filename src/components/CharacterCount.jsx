f3df487a009549e859a1903aa8667b0c0ba1eac6
Riudiu <lacvert13@gmail.com>
2023-07-24 17:35:02 +0900

    주석 추가

--- src/components/CharacterCount.jsx
+++ src/components/CharacterCount.jsx
@@ -2,6 +2,8 @@ import { useRecoilValue } from 'recoil';
 import { charCountState } from '../App'
 
 export function CharacterCount() {
+  // useRecoilValue() - 전역 상태의 state 상태 값만을 참조하기 위해 사용
+  // 선언된 변수에 할당하여 사용하면 된다.
   const count = useRecoilValue(charCountState);
   return (
     <div>
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -3,6 +3,8 @@ import { useRecoilState } from 'recoil'
 import { textState } from '../App'
 
 export function TextInput() {
+    // useRecoilState() - useState()와 유사하며, [state, useState] 튜플을 반환
+    // 인자에 Atom 혹은 Selector를 넣어준다.
     const [text, setText] = useRecoilState(textState);
     const onChange = (event) => {
         setText(event.target.value);
